5dd6fa141df2e79e7c692feefab089b6b8f6dc30
aadiahlawat06-beep <aadiahlawat06@gmail.com>
2026-09-16 09:44:57 +0530

    Add files via upload
    My Binary Search code

---
+++ binary_search.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], i, loc, mid, beg, end, n, flag = 0, item;
+
+    printf("How many elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array\n");
+
+    for(i = 0; i <= n - 1; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the element to be searched\n");
+    scanf("%d", &item);
+
+    loc = 0;
+    beg = 0;
+    end = n - 1;
+
+    while(beg <= end)
+    {
+        mid = (beg + end) / 2;
+
+        if(item == a[mid])
+        {
+            printf("Search is successful\n");
+
+            loc = mid;
+
+            printf("Position of the item %d\n", loc + 1);
+
+            flag = flag + 1;
+
+            break;
+        }
+        else if(item < a[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            beg = mid + 1;
+        }
+    }
+
+    if(flag == 0)
+    {
+        printf("Search is not successful\n");
+    }
+
+    return 0;
+}
