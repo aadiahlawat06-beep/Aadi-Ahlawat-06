858396ce7ab076a1345a7b97466171eccaff8294
aadiahlawat06-beep <aadiahlawat06@gmail.com>
2026-09-23 09:12:17 +0530

    Add files via upload

---
+++ display.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    display(arr, n);
+
+    return 0;
+}
