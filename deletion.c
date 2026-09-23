6f4378cb6511b074629feea83b875c86bf49529f
aadiahlawat06-beep <aadiahlawat06@gmail.com>
2026-09-23 09:12:00 +0530

    Add files via upload

--- deletion.c
+++ deletion.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    int arr[10] = {10, 20, 30, 40, 50};
-    int n = 5, pos = 3;
+int deleteElement(int arr[], int n, int x) {
     int i;
-
-    for(i = pos - 1; i < n - 1; i++) {
-        arr[i] = arr[i + 1];
+    for (i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            break;
+        }
     }
 
-    n--;
+    if (i < n) {
+        n--;
+        for (int j = i; j < n; j++) {
+            arr[j] = arr[j + 1];
+        }
+    }
 
-    for(i = 0; i < n; i++) {
+    return n;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 30;
+
+    n = deleteElement(arr, n, x);
+
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
 
     return 0;
 }
